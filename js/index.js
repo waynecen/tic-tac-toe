533d7e60983faf023d94797fb642965cfc7e43d5
Wayne <wayne.cen@gmail.com>
2022-01-11 21:33:36 -0500

    Setup HTML CSS JS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// I want a module for the gameBoard and displayController
+let gameBoard = (function() => {
+    const 
+})
+
+// I want a factory function for players
+
+const players = ((name, playerType) => {
+    return { name, playerType };
+})();
+
